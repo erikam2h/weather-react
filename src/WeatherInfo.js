f22a7e7e8ec8cfce649add296a83fefd72a30f17
erikam2h <erikam.mh90@gmail.com>
2022-02-06 00:28:02 -0600

    Change kind of temperature from Celsius to Fahrenheit. And fixed design problems

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormatDate from "./FormatDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherUnits from "./WeatherUnits";
 
 export default function WeatherInfo(props) {
   return (
@@ -9,22 +10,25 @@ export default function WeatherInfo(props) {
         <h3 className="city-name">{props.data.city}</h3>
 
         <FormatDate date={props.data.date} />
-
         <ul className="humidity-wind ">
           <li>Precipitation: 15%</li>
           <li>Humidity: {props.data.humidity}%</li>
           <li>Wind: {props.data.wind} km/h</li>
         </ul>
       </div>
-      <div className="col-lg-2 col-sm-6">
-        <div className="weather-lg text-center">
-          {props.data.temperature}
-          <span className="grados-link">°C</span>
+      <div className="col-lg-4 col-sm-12">
+        <div className="row">
+          <div className="col-lg-4 col-sm-6 mt-4">
+            <WeatherIcon code={props.data.icon} />
+          </div>
+          <div className="col-lg-8 col-sm-6">
+            <WeatherUnits celsius={props.data.temperature} />
+          </div>
+          <hr />
+          <p className="description text-capitalize text-center">
+            {props.data.description}
+          </p>
         </div>
-      </div>
-      <div className="col-lg-2 col-sm-6 mt-4">
-        <WeatherIcon code={props.data.icon} />
-        <p className="description text-capitalize">{props.data.description}</p>
       </div>
     </div>
   );
