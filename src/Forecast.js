4e88bd01617e620245376e2659ef41bfa70a9920
erikam2h <erikam.mh90@gmail.com>
2022-02-06 21:43:31 -0600

    forecast api integration and format day 1

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,33 +1,33 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
+
+import ForecastDay from "./ForecastDay";
 import axios from "axios";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponseF(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+
+    setLoaded(true);
   }
   console.log(props);
 
-  let apiKey = "62443ec44a5ca2da39a6b31ebb5a82c4";
-  let lon = props.coordinates.lon;
-  let lat = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponseF);
-
-  return (
-    <div className="row justify-content-md-center mt-4">
-      <div className="col-lg-2 col-sm-4 box-day">
-        <p className="day">Monday</p>
-        <p className="text-center">
-          <WeatherIcon code="01d" size={45} />
-        </p>
-        <hr className="box-line" />
-        <p className="text-center">
-          <span className="forecast-min">7°C </span> |
-          <span className="forecast-max"> 28°C</span>
-        </p>
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="row justify-content-md-center mt-4">
+        <ForecastDay dataForecast={forecast[0]} />
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "62443ec44a5ca2da39a6b31ebb5a82c4";
+    let lon = props.coordinates.lon;
+    let lat = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponseF);
+    return null;
+  }
 }
